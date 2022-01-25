884f0b33109f64ea7b90f6476c07a542b6efaf41
gahyunkim <mapledt001@naver.com>
2022-01-26 03:45:53 +0900

    회원가입 조건 추가1

--- app/src/main/java/com/example/potfollio/SignUpActivity.kt
+++ app/src/main/java/com/example/potfollio/SignUpActivity.kt
@@ -15,6 +15,7 @@ class SignUpActivity : AppCompatActivity() {
     lateinit var edtName : EditText
     lateinit var edtId: EditText
     lateinit var edtPass: EditText
+    lateinit var edtRePass: EditText
     lateinit var btnSign: Button
     lateinit var sqlDB: SQLiteDatabase
     lateinit var dbManager: DBManager
@@ -26,44 +27,105 @@ class SignUpActivity : AppCompatActivity() {
         edtName = findViewById(R.id.edtName)
         edtId = findViewById(R.id.edtId)
         edtPass = findViewById(R.id.edtPass)
+        edtRePass = findViewById(R.id.edtRePass)
         btnSign = findViewById(R.id.btnSign)
 
         dbManager = DBManager(this,"groupTBL",null,1)
+        sqlDB = dbManager.writableDatabase
 
         btnSign.setOnClickListener {
-            if(edtName.text.toString().isBlank()||edtId.text.toString().isBlank()|| edtPass.text.toString().isBlank()){
-                // 닉네임, 아이디, 비번은 필수 입력 사항
-                // (수정 완료) 수정필요: 이름,id,pw 모두 입력해야 넘어가는 걸로..
-                Toast.makeText(applicationContext,"닉네임, 아이디와 비밀번호를 입력해주세요.", Toast.LENGTH_SHORT).show()
-            }
-            else{
-                // 회원가입시에 DB에 내용 저장
-                sqlDB = dbManager.writableDatabase
-                sqlDB.execSQL("INSERT INTO groupTBL VALUES ( '"
-                        + edtName.text.toString()+ "' , '"
-                        + edtId.text.toString() + "' , '"
-                        + edtPass.text.toString()+"');")
-                sqlDB.close()
+            sqlDB = dbManager.writableDatabase
+            var cursor: Cursor
+            cursor = sqlDB.rawQuery("SELECT gName,gID FROM groupTBL", null)
 
-                // 회원가입시에 토스트 메시지 전달
-                Toast.makeText(applicationContext,"회원가입이 완료되었습니다.",Toast.LENGTH_SHORT).show()
+            if(cursor.count==0){
+                // 데이터베이스에 아무런 행도 없는 경우 = 아직 값이 들어오지 않은 경우
+                if(edtName.text.toString().isBlank()||edtId.text.toString().isBlank()|| edtPass.text.toString().isBlank()||edtRePass.text.toString().isBlank()){
+                    // 닉네임, 아이디, 비번은 필수 입력 사항
+                    // (수정 완료) 수정필요: 이름,id,pw 모두 입력해야 넘어가는 걸로..
+                    Toast.makeText(applicationContext,"닉네임, 아이디와 비밀번호를 입력해주세요.", Toast.LENGTH_SHORT).show()
+                }
+                else if(edtName.text.toString().length < 2){
+                    Toast.makeText(applicationContext,"닉네임을 2자리 이상 입력하세요", Toast.LENGTH_SHORT).show()
+                }
+                else if(edtId.text.toString().length< 6){
+                    Toast.makeText(applicationContext,"아이디를 6자리 이상 입력하세요.", Toast.LENGTH_SHORT).show()
+                }
+                else if(edtPass.text.toString().length< 6){
+                    Toast.makeText(applicationContext,"비밀번호를 6자리 이상 입력하세요.", Toast.LENGTH_SHORT).show()
+                }
+                else if(edtRePass.text.toString()!=edtPass.text.toString()){
+                    Toast.makeText(applicationContext,"비밀번호가 다릅니다. 다시 입력하세요", Toast.LENGTH_SHORT).show()
+                }
+                else{
+                    sqlDB.execSQL(
+                        "INSERT INTO groupTBL VALUES ( '"
+                                + edtName.text.toString() + "' , '"
+                                + edtId.text.toString() + "' , '"
+                                + edtPass.text.toString() + "');"
+                    )
+                    sqlDB.close()
+                    // 회원가입시에 토스트 메시지 전달
+                    Toast.makeText(applicationContext, "회원가입이 완료되었습니다.", Toast.LENGTH_SHORT).show()
 
-                // 로그인화면으로 전환
-                var intent = Intent(this, LoginActivity::class.java)
-                startActivity(intent)
+                    // 로그인화면으로 전환
+                    var intent = Intent(this, LoginActivity::class.java)
+                    startActivity(intent)
+                }
             }
-            // 추가할 부분 = 회원가입 시에 사용자들이 서로 동일한 닉네임을 사용할 수 없도록 함. 따라서 사용하고자하는 닉네임이 이미 존재하는 경우에는 막아야함
-//            //sqlDB = dbManager.readableDatabase
-//
-//            var cursor: Cursor
-//            cursor = sqlDB.rawQuery("SELECT gName FROM groupTBL", null)
-//
-//            while(cursor.moveToNext()){
-//                var strName = cursor.getString(0)
-//                if (strName == edtName.text.toString()) {
-//                    Toast.makeText(applicationContext, "사용 중인 닉네임입니다.", Toast.LENGTH_SHORT).show()
-//                }
-//            }
+
+            while(cursor.moveToNext()){
+                var strName = cursor.getString(0)
+                var strId = cursor.getString(1)
+
+                if(edtName.text.toString().isBlank()||edtId.text.toString().isBlank()|| edtPass.text.toString().isBlank()||edtRePass.text.toString().isBlank()){
+                    // 닉네임, 아이디, 비번은 필수 입력 사항
+                    // (수정 완료) 수정필요: 이름,id,pw 모두 입력해야 넘어가는 걸로..
+                    Toast.makeText(applicationContext,"닉네임, 아이디와 비밀번호를 입력해주세요.", Toast.LENGTH_SHORT).show()
+                    break
+                }
+                else if(edtName.text.toString().length < 2){
+                    Toast.makeText(applicationContext,"닉네임을 2자리 이상 입력하세요", Toast.LENGTH_SHORT).show()
+                    break
+                }
+                else if(strName == edtName.text.toString()){
+                    Toast.makeText(applicationContext,"이미 사용 중인 닉네임입니다.", Toast.LENGTH_SHORT).show()
+                    break
+                }
+                else if(strId == edtId.text.toString()){
+                    Toast.makeText(applicationContext,"이미 사용 중인 아이디입니다.", Toast.LENGTH_SHORT).show()
+                    break
+                }
+                else if(edtId.text.toString().length< 6){
+                    Toast.makeText(applicationContext,"아이디를 6자리 이상 입력하세요.", Toast.LENGTH_SHORT).show()
+                    break
+                }
+                else if(edtPass.text.toString().length< 6){
+                    Toast.makeText(applicationContext,"비밀번호를 6자리 이상 입력하세요.", Toast.LENGTH_SHORT).show()
+                    break
+                }
+                else if(edtRePass.text.toString()!=edtPass.text.toString()){
+                    Toast.makeText(applicationContext,"비밀번호가 다릅니다. 다시 입력하세요", Toast.LENGTH_SHORT).show()
+                }
+                else{
+                    sqlDB.execSQL(
+                        "INSERT INTO groupTBL VALUES ( '"
+                                + edtName.text.toString() + "' , '"
+                                + edtId.text.toString() + "' , '"
+                                + edtPass.text.toString() + "');"
+                    )
+                    sqlDB.close()
+
+                    // 회원가입시에 토스트 메시지 전달
+                    Toast.makeText(applicationContext, "회원가입이 완료되었습니다.", Toast.LENGTH_SHORT).show()
+
+                    // 로그인화면으로 전환
+                    var intent = Intent(this, LoginActivity::class.java)
+                    startActivity(intent)
+                    break
+                }
+                cursor.close()
+            }
         }
     }
 
@@ -73,9 +135,8 @@ class SignUpActivity : AppCompatActivity() {
                     version: Int
     ) : SQLiteOpenHelper(context,name,factory,version){
         override fun onCreate(db: SQLiteDatabase?){
-            db!!.execSQL("CREATE TABLE groupTBL ( gName CHAR(20) PRIMARY KEY, gID CHAR(30) NOT NULL, gPass CHAR(30) NOT NULL);")
+            db!!.execSQL("CREATE TABLE groupTBL ( gName CHAR(20) NOT NULL, gID CHAR(30) NOT NULL, gPass CHAR(30) NOT NULL);")
         }
-
         override fun onUpgrade(db: SQLiteDatabase?,oldversion: Int, newVersion: Int){
 
         }
@@ -86,7 +147,7 @@ class SignUpActivity : AppCompatActivity() {
 //        }
 //        override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
 //            db!!.execSQL("DROP TABLE IF EXISTS groupTBL")
-//            onCreate(db)
+//            onCreate(db)c
 //        }
 //    }
 }
