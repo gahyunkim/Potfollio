1f03c95550305ebefc04ba8c666ae174d4d45ae5
lkeyu <lkeyun3301@gmail.com>
2022-02-02 16:44:56 +0900

    settings and expain

---
+++ app/src/main/java/com/example/potfollio/ExpainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.potfollio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ExpainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_expain)
+    }
+}
